7e3d43030107158fc675db798f6ab9c23d42ae93
Nasandratra Harivonjy <nasandratra.access4@gmail.com>
2022-10-29 21:52:27 -0700

    done

--- Week7/exercise/main.js
+++ Week7/exercise/main.js
@@ -3,7 +3,7 @@ fetch(url)
     .then(res => res.json())
     .then(quiz => {
         view.start.addEventListener('click', () =>
-            game.start(quiz), false);
+            game.start(quiz.questions), false);
         view.response.addEventListener('click', (event) =>
             game.check(event), false);
 });
